8eea3e6654592e40938175fd1916cb35ed66ef88
Jan Hoffmann <jan.hoffmann@check24.de>
2025-07-07 17:14:36 +0200

    initial activation and readd old configs

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -16,6 +16,29 @@ return {
     config = function() require("lsp_signature").setup() end,
   },
 
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "BufEnter",
+    config = function()
+      require("copilot").setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<C-f>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        copilot_node_command = "node", -- Node.js version must be > 16.x
+        server_opts_overrides = {},
+      }
+    end,
+  },
+
   -- == Examples of Overriding Plugins ==
 
   -- customize dashboard options
